cceb989ace6acb83ba36e405ccf08361a68c2e18
liubas3171 <54552855+liubas3171@users.noreply.github.com>
2020-05-16 17:28:04 +0300

    Update example_adt.py

--- examples/example_adt.py
+++ examples/example_adt.py
@@ -1,14 +1,13 @@
 '''Module for representing how to work with TweetAnalyser'''
 
-from home_tasks.hometask3.abstract_data_type import TweetAnalyser
+import TweetAnalyser
 
-analyser = TweetAnalyser('D:/UCU/Програмування/2 семестр/home_tasks/hometask3/TWEET_METADATA.json')
+analyser = TweetAnalyser('Hometasks/examples/TWEET_METADATA.json')
 # collecting all retweets
 analyser.collect_retweet(original=False, there=False)
-# managing what parameters of tweet we will live
+# managing what parameters of tweet we will keep
 analyser.manage_what_live({'created_at', 'text', 'user:name',
                            'user:screen_name', 'retweeted_status:user:name',
                            'retweeted_status:user:screen_name'})
 # saving analysed tweets to file
-analyser.save('D:/UCU/Програмування/2 семестр/home_tasks/hometask3/analysed_tweets.json')
-
+analyser.save('Hometasks/examples/analysed_tweets.json')
